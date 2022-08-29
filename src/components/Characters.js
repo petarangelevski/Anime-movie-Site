d434c1b8bd7e25835f5b5ca94ed75c54e5b8030e
petarangelevski <petarangelevski25@gmail.com>
2022-08-29 22:57:31 +0200

    .

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -6,7 +6,7 @@ import './Characters.css';
 function Characters() {
   const [allCharacters, setAllCharacters] = useState([]);
   const [largeImage, setLargeImage] = useState([]);
-  const [about, setAbout]=useState([]);
+
 
   let navigate = useNavigate();
 
@@ -15,25 +15,37 @@ function Characters() {
   };
 
   const { id } = useParams();
-
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(URL);
       const result1 = await axios(
         URL
       );
-      const result2 = await axios (URL);
+   
 
 
       const results = result.data.data;
       const results1 = result.data.data.images.jpg.image_url
-      const results2 = result.data.data.about;
-      setAbout(results2)
-      setLargeImage(results1);
+    
       setAllCharacters(results);
+    
+      setLargeImage(results1);
     };
     fetchData();
   }, []);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   const URL = `https://api.jikan.moe/v4/characters/${id}/full`;
 
@@ -49,22 +61,24 @@ function Characters() {
           </div>
           <div className="single__image__card__text">
             <div className="z-index">
-              <div className="z-index-naslov">
-                <strong>{allCharacters.name}</strong>
-              </div>
+
             </div>
           </div>
-          <div className="btn-container">
+
+        </div>
+      </div>
+      <div className="about">
+      <div className="z-index-naslov">
+                <strong>{allCharacters.name}</strong>
+              </div>
+        <div className="txtAbout">
+          <p>{allCharacters.about}</p>
+        </div>
+        <div className="btn-container">
             <button className="single__image__button" onClick={routeChange}>
               <p className="single__image__button__text">go back</p>
             </button>
           </div>
-        </div>
-      </div>
-      <div className="about">
-        <div className="txtAbout">
-          <p>{about}</p>
-        </div>
       </div>
     </div>
   );
